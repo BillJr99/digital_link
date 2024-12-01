67b5f923e78d072a4e7cf067adb41f956de358f6
William Mongan <billmongan@gmail.com>
2024-12-01 18:52:42 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,90 @@
+# DTMF Digital Mode Changer for DVSwitch 
+
+William M. Mongan
+December 1, 2024
+
+## Installation and Configuration
+
+Clone this repository to your AllStar node `/opt` directory, via:
+
+```
+cd /opt
+git clone https://github.com/BillJr99/digital_link
+```
+
+Within the `digital_link` directory, there is a minimal amount of configuration.  Copy the file `switch_modes.conf.sample` to `switch_modes.conf`, and create as many sections as you like.  Some modes do not need all the fields, and these are set to NA in the template.  The `modemaster` defines the first two DTMF tones one will send to link to that mode and master.  For example, `12` will select the TGIF network on DMR, since `1` represents DMR, and `2` represents TGIF. Ensure that all masters within the same mode have the same first `modemaster` digit: for example, all DMR networks should have a first `modemaster` digit of `1`, and all D-STAR networks should have a first `modemaster` digit of `2`.  The actual digit is your choice, as long as you do not select `0` for the mode or the master digit (these are reserved for unlinking).  Set the `mode` to `DMR`, `D-STAR`, `YSF`, `NXDN`, or `P25` as appropriate, as well as the host, port, and password, if applicable.
+
+Within `digital_link.sh`, set your AllStar node number and linked digital node (i.e., `1999`) for `NODE_ID` and `LINKED_NODE_ID`, respectively.
+
+Finally, ensure that `/opt/digital_link/dvswitch_links.log` is owned by the `asterisk` user, and that the `digital_link.sh` script is executable:
+
+```
+sudo touch dvswitch_links.log
+sudo chown asterisk:asterisk dvswitch_links.log
+chmod a+x digital_link.sh
+```
+
+## Mapping to Asterisk
+
+You can assign a dialplan to Asterisk so that the DTMF tones for a given code pattern will be forwarded to this script.  I used `AA` as my entry code, and recommend choosing the same two-digit/character sequence.  This is important because the `rpt.conf` will read and consume the first digit, and then pass the remaining to the dialplan.  The second digit allows the dialplan to match the rest of the string, in case you have other dialplan patterns in place.  This resulting string (starting with a single `A`) is passed to the script for parsing.
+
+### `/etc/asterisk/rpt.conf`
+
+Modify `rpt.conf` under the `[functions]` section by adding the following line (replace `A` with the DTMF tone you'd like to match to run the script):
+
+```
+A = autopatchup,context=digital_link,noct=1,farenddisconnect=1,dialtime=60000,quiet=1
+```
+
+### `/etc/asterisk/extensions.conf`
+
+Modify `extensions.conf` by adding the following stanza (again, replace `_A.` with `_@.`, where `@` is the DTMF tone you used in `rpt.conf` above):
+
+```
+[digital_link]
+; Match extensions explicitly ending with D
+exten => _A.,1,GotoIf($["${EXTEN:-1}"="D"]?ends_with_D:normal)
+; If it ends with D, replace the last character with #
+exten => _A.,n(ends_with_D),System(/opt/digital_link/digital_link.sh ${EXTEN:0:-1}#)
+exten => _A.,n,Hangup
+; If it does not end with D, process normally
+exten => _A.,n(normal),System(/opt/digital_link/digital_link.sh ${EXTEN})
+exten => _A.,n,Hangup
+```
+
+## Usage
+
+Assuming you used `A` to start the `autopatch` and for the `exten` rules above, you can send the following DTMF tones to your AllStarLink node to activate DVSwitch, change/unlink modes, and change/unlink talkgroups.  All commands will begin with `*AA`.  The first `A` is provided to Asterisk, the second `A` is matched by the autopatch extension rules, and the entire string starting with (and including) the second `A` is passed to the `digital_link.sh` script and parsed.
+
+```
+*AA <mode digit> <master> <optional TG> <optional D key>
+```
+
+* **<mode digit>:** The mode digit is the first digit in `switch_modes.conf` that corresponds to the `modemaster` of the master and/or talkgroup you wish to connect to.  For example, DMR corresponds to `1`, and D-STAR corresponds to `2` in the template provided by `switch_modes.conf`.
+* **<master>:** The master digit is the second digit in `switch_modes.conf` that corresponds to the `modemaster` of the master and/or talkgorup you wish to connect to.  For example, Brandmeister corresponds to `1` and TGIF corresponds to `2` for DMR, and REF corresponds to `1` and XLX corresponds to `2` for D-STAR.  
+* **<TG>:** The talkgroup you wish to connect to; for example, 9999.  This can be omitted if there is no specific talkgroup on the master being connected to (i.e., when using YSF).  On D-STAR, this is the numeric component, so to connect to `REF030C`, one would press the digit corresponding to `REF` for the `<master>` digit previously, and then enter `030C`.
+* **<optional D key>:** Sending the `D` DTMF tone at the end of the command indicates that a private call should be made on DMR, or to link to the `Echo` repeater on D-STAR.  This is translated to a `#` character and passed to DVSwitch as either a escaped `#` character, or as the `E` echo tone on D-STAR, as appropriate.
+
+### Linking
+
+The first time you issue one of these commands, Asterisk will unlink from any connected nodes other than your digital-analog bridge node (i.e., `1999`), and link to your digital-analog bridge node if it wasn't already linked.
+
+### Examples
+
+These examples assume the stock configuration in `digital_link.sh`, with spaces added to the commands for clarity (note that the entire set of tones is entered as a single transmission):
+
+* Connect to REF030C on D-STAR: `*AA 21 030C`
+* Connect to REF001E on D-STAR: `*AA 21 001D`
+* Connect to 9990 (Private call) on DMR Brandmeister: `*AA 11 9990D`
+* Connect to TG 91 on DMR Brandmeister: `*AA 11 91`
+* Connect to Parrot on YSF: `*AA 31`
+* Connect to AmericaLink on YSF: `*AA 32`
+* Connect to TG 101 on DMR TGIF: `*AA 12 101`
+
+### Unlinking
+
+When you switch modes, the unlink command for the current mode is issued.  You can force an unlink by entering `*AA 0`.  If you wish to unlink from the current digital mode and also unlink from your digital-analog bridge node entirely on AllStar, you can enter `*AA 00`.
+
+## Disclaimer
+
+This software is only lightly tested and should be considered in beta.  Your testing, feedback, and contributions are most welcome.  Please be sure to have a backup of your configurations, and be sure to check that the modes are correctly linked / unlinked!
---
+++ digital_link.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+# Run as asterisk or root
+
+# Node configuration
+NODE_ID=62933          # Allstar node number
+LINKED_NODE_ID=1999    # Target private node to be linked
+CONFIG_FILE="/opt/digital_link/switch_modes.conf"
+LOG_FILE="/opt/digital_link/dvswitch_links.log"
+DVSWITCH_APP="/opt/MMDVM_Bridge/dvswitch.sh"
+
+echo "$(date): Executing with $1" >> $LOG_FILE
+
+# Input DTMF string
+DTMF=$1
+DTMF=${DTMF:1} # remove the initial digit to Asterisk
+DTMF=${DTMF//\*/\#} # replace * with # for parsing purposes
+
+# Validate input
+if [[ -z "$DTMF" || ${#DTMF} -lt 2 ]]; then
+    echo "Invalid DTMF command. Exiting."
+    echo "$(date): Invalid DTMF command ${DTMF}" >> $LOG_FILE
+    exit 1
+fi
+
+MODE_DIGIT=${DTMF:0:1}   # First digit (mode)
+MASTER=${DTMF:1:1}       # Second digit (master)
+TG=${DTMF:2}             # Remaining digits (talkgroup ID)
+
+unlink_current_mode() {
+    # Constants
+    DMR_UNLINK=4000
+    DSTAR_UNLINK=U
+    YSF_UNLINK=4000
+    NXDN_UNLINK=9999
+    P25_UNLINK=9999
+
+    # Unlink current mode
+    local current_mode=$(${DVSWITCH_APP} mode)
+
+    case $current_mode in
+        DMR)
+            ${DVSWITCH_APP} tune ${DMR_UNLINK}
+            ;;
+        DSTAR)
+            ${DVSWITCH_APP} tune ${DSTAR_UNLINK}
+            ;;
+        YSF|YSFN)
+            ${DVSWITCH_APP} tune ${YSF_UNLINK}
+            ;;
+        NXDN)
+            ${DVSWITCH_APP} tune ${NXDN_UNLINK}
+            ;;
+        P25)
+            ${DVSWITCH_APP} tune ${P25_UNLINK}
+            ;;
+        *)
+            echo "WARNING: Unable to unlink unsupported mode: $current_mode"
+            ;;
+    esac
+
+    echo Unlinked on mode $current_mode
+    echo "$(date): Unlinked on mode $current_mode" >> $LOG_FILE
+}
+
+# Disconnect and Unlink only when MODE and MASTER are both 0
+if [[ "$MODE_DIGIT" == "0" && "$MASTER" == "0" ]]; then
+    echo "Disconnecting node $NODE_ID from AllStarLink..."
+
+    # Unlink current mode
+    unlink_current_mode   
+
+    # Disconnect NODE_ID from LINKED_NODE_ID
+    asterisk -rx "rpt cmd $NODE_ID ilink 1 ${LINKED_NODE_ID}"
+
+    echo "Node $NODE_ID disconnected from AllStarLink."
+    echo "$(date): Disconnected node $NODE_ID" >> $LOG_FILE
+    
+    exit 0
+fi
+
+# Unlink from current mode when MODE is 0
+if [[ "$MODE_DIGIT" == "0" ]]; then
+    echo "Unlinking only"
+    
+    # Unlink current mode
+    unlink_current_mode
+
+    echo "Node $NODE_ID unlinked."
+    echo "$(date): Unlinked from current node" >> $LOG_FILE
+    
+    exit 0
+fi
+
+# Ensure NODE_ID is connected to LINKED_NODE_ID
+CURRENT_CONNECTION=$(asterisk -rx "rpt lstats $NODE_ID" | grep "ESTABLISHED")
+if [[ -n "$CURRENT_CONNECTION" && "$CURRENT_CONNECTION" != *"$LINKED_NODE_ID"* ]]; then
+    echo "Disconnecting from other nodes..."
+    asterisk -rx "rpt cmd $NODE_ID ilink 6 1"
+fi
+if [[ "$CURRENT_CONNECTION" != *"$LINKED_NODE_ID"* ]]; then
+    echo "Connecting node $NODE_ID to $LINKED_NODE_ID..."
+    asterisk -rx "rpt fun $NODE_ID *3$LINKED_NODE_ID"
+    sleep 2
+fi
+
+# Find the configuration section matching the master value
+SECTION=$(awk -v modemaster="${MODE_DIGIT}${MASTER}" '
+    /^\[.*\]/ { section=$0; in_section=0 }   # Capture section name and reset in_section
+    $0 ~ "modemaster="modemaster { in_section=1 }  # Match modemaster exactly
+    in_section { print section; exit }      # Print section name and exit when in_section is set
+' "$CONFIG_FILE")
+
+if [[ -z "$SECTION" ]]; then
+    echo "No matching configuration for MASTER=${MASTER}. Exiting."
+    exit 1
+fi
+
+# Extract details from the matching section
+MODE=$(awk -v section="$SECTION" '
+    $0 == section { in_section=1; next }  # Enter the section
+    in_section && /^\[.*\]/ { exit }      # Exit on the next section
+    in_section && $1 ~ /^mode=/ { split($0, a, "="); print a[2]; exit }
+' "$CONFIG_FILE")
+
+URL=$(awk -v section="$SECTION" '
+    $0 == section { in_section=1; next }
+    in_section && /^\[.*\]/ { exit }
+    in_section && $1 ~ /^url=/ { split($0, a, "="); print a[2]; exit }
+' "$CONFIG_FILE")
+
+PORT=$(awk -v section="$SECTION" '
+    $0 == section { in_section=1; next }
+    in_section && /^\[.*\]/ { exit }
+    in_section && $1 ~ /^port=/ { split($0, a, "="); print a[2]; exit }
+' "$CONFIG_FILE")
+
+PASSWORD=$(awk -v section="$SECTION" '
+    $0 == section { in_section=1; next }
+    in_section && /^\[.*\]/ { exit }
+    in_section && $1 ~ /^password=/ { split($0, a, "="); print a[2]; exit }
+' "$CONFIG_FILE")
+
+TYPE=$(awk -v section="$SECTION" '
+    $0 == section { in_section=1; next }
+    in_section && /^\[.*\]/ { exit }
+    in_section && $1 ~ /^type=/ { split($0, a, "="); print a[2]; exit }
+' "$CONFIG_FILE")
+
+# Unlink current mode
+unlink_current_mode  
+    
+# Select the mode and tune the talkgroup
+echo "Switching to mode $MODE, MASTER=${MASTER}, TG: $TG, URL: $URL, PORT: $PORT, PASSWORD: $PASSWORD, TYPE: $TYPE"
+
+if [[ "$MODE" == "D-STAR" ]]; then
+    if [[ "${TG: -1}" == "#" ]]; then
+        TG="${TG%#}E"  # Remove the last character (#) and append E
+    fi
+    TG="${TYPE}${TG}L"  # Always prepend $TYPE (REF, XLX, ...) and append L for D-STAR
+fi
+
+case $MODE in
+    DMR)
+        ${DVSWITCH_APP} mode DMR
+        echo Executing: ${DVSWITCH_APP} mode DMR
+        ${DVSWITCH_APP} tune ${PASSWORD}@${URL}:${PORT}\!${TG}
+        echo Executing: ${DVSWITCH_APP} tune ${PASSWORD}@${URL}:${PORT}\\\!${TG}
+        ;;
+    D-STAR)
+        ${DVSWITCH_APP} mode DSTAR
+        echo Executing: ${DVSWITCH_APP} mode DSTAR
+        ${DVSWITCH_APP} tune ${TG}
+        echo Executing: ${DVSWITCH_APP} tune ${TG}
+        ;;
+    YSF)
+        ${DVSWITCH_APP} mode YSF
+        echo Executing: ${DVSWITCH_APP} mode YSF
+        ${DVSWITCH_APP} tune ${URL}:${PORT}
+        echo Executing: ${DVSWITCH_APP} tune ${URL}:${PORT}
+        ;;
+    NXDN)
+        ${DVSWITCH_APP} mode NXDN
+        echo Executing: ${DVSWITCH_APP} mode NXDN
+        ${DVSWITCH_APP} tune ${TG}
+        echo Executing: ${DVSWITCH_APP} tune ${TG}
+        ;;
+    P25)
+        ${DVSWITCH_APP} mode P25
+        echo Executing: ${DVSWITCH_APP} mode P25
+        ${DVSWITCH_APP} tune ${TG}
+        echo Executing: ${DVSWITCH_APP} tune ${TG}
+        ;;        
+    *)
+        echo "Unsupported mode: $MODE"
+        exit 1
+        ;;
+esac
+
+# Log the action
+echo "$(date): Connected to node $NODE_ID, Mode=$MODE, Master=$MASTER, TG=$TG" >> $LOG_FILE 
